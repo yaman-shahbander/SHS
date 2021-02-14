45c633aeca9ac12df1bc3978d63bdaaa70a05e01
Mousa-Sa <xdggasdg@gmail.com>
2021-02-14 17:46:45 +0200

    APIs edits

--- database/migrations/2020_03_27_094855_create_notifications_table.php
+++ database/migrations/2020_03_27_094855_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationsTable extends Migration
+class CreateNotificationsTable_ extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('notifications');
-        Schema::dropIfExists('notification_types');
-        Schema::create('notifications', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('type');
-            $table->morphs('notifiable');
-            $table->text('data');
-            $table->timestamp('read_at')->nullable();
-            $table->timestamps();
-        });
+//        Schema::dropIfExists('notifications');
+//        Schema::dropIfExists('notification_types');
+//        Schema::create('notifications', function (Blueprint $table) {
+//            $table->uuid('id')->primary();
+//            $table->string('type');
+//            $table->morphs('notifiable');
+//            $table->text('data');
+//            $table->timestamp('read_at')->nullable();
+//            $table->timestamps();
+//        });
     }
 
     /**
