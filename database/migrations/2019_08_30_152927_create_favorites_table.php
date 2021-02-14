7ce0bdc8d60e7cfcc1b51ad89ae30cf86ac4a391
Mousa-Sa <xdggasdg@gmail.com>
2021-02-14 17:17:04 +0200

    APIs edits

--- database/migrations/2019_08_30_152927_create_favorites_table.php
+++ database/migrations/2019_08_30_152927_create_favorites_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateFavoritesTable extends Migration
+class CreateFavoritesTable_ extends Migration
 {
 
     /**
