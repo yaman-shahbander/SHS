698dc4b5ced7fbe2d38d4f7d0e9b66a5c3bdaa1d
Mousa-Sa <xdggasdg@gmail.com>
2021-02-14 17:18:39 +0200

    APIs edits

--- database/migrations/2019_09_22_192348_create_messages_table.php
+++ database/migrations/2019_09_22_192348_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagesTable extends Migration
+class CreateMessagesTable_ extends Migration
 {
     /**
      * Run the migrations.
